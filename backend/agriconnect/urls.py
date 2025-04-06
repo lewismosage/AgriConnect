3177fc883c7d99c8d7aa2514e677197cb63c6433
Lewismosage <lewismosage@users.noreply.github.com>
2025-04-06 18:01:28 +0300

    New Updates

--- backend/agriconnect/urls.py
+++ backend/agriconnect/urls.py
@@ -1,10 +1,10 @@
 # agriconnect/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from farms.views import SearchView
+from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
